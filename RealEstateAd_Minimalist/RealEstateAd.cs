19b310ffc910db5991b5a595616452af223b2734
Matthieu BROSSAULT <ext.matthieu.brossault@bollore.com>
2022-12-15 22:20:35 +0100

    Add domain entities

---
+++ RealEstateAd_Minimalist/RealEstateAd.cs
@@ -0,0 +1,25 @@
+﻿namespace RealEstateAd_Minimalist
+{
+    public enum PropertyType
+    {
+        House = 0,
+        Apartment = 1,
+        Parking = 2,
+    }
+
+    public enum PublishStatus
+    {
+        WaitingValidation = 0,
+        Published = 1,
+    }
+
+    public class RealEstateAd
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Localisation { get; set; } = string.Empty;
+        public PropertyType PropertyType { get; set; }
+        public PublishStatus PublishStatus { get; set; }
+    }
+}
